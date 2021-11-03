54eb3426e7c193e49b31e59de37308010114a689
freeomsk <freeomsk@yandex.ru>
2021-11-03 09:50:48 +0600

    Code style

--- src/main/java/web/controller/AdminController.java
+++ src/main/java/web/controller/AdminController.java
@@ -1,13 +1,12 @@
 package web.controller;
 
-import web.model.User;
-import web.service.RoleService;
-import web.service.UserService;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
+import web.model.User;
+import web.service.RoleService;
+import web.service.UserService;
 import javax.validation.Valid;
 import java.util.ArrayList;
 
